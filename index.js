ac5aa50f2bef02939f9d7218a9c867fd283d1562
DamianM <damian.michalski@kramp.com>
2019-01-29 13:51:59 +0100

    fs.readdir fs.writeFile

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var colors = require('colors');
 fs.readdir('./', function(err, data) {
-  if (err) throw err;
+    if (err) throw err;
     console.log('Dane przed zapisem!'.blue);
     console.log(data);
     fs.writeFile('./tekstNowy.txt', data, function(err) {
